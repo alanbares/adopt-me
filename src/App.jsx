5f43c035119d337a15dc2b47db752346a20d7dc2
Alan Bares <alanbares@gmail.com>
2023-10-11 15:43:48 -0700

    Update spaces. Add SearchParams and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 
 const App = () => {
- <div>
-  <h1>Adopt Me!</h1>
-  <Pet name ="Luna" animal="dog" breed="Havanese" />
-  <Pet name ="Missile" animal="dog" breed="Corgi" />
-  <Pet name ="Meowy" animal="cat" breed="Magpie" />
- </div>
+  return(
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");

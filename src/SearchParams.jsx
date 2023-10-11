d3a38c1f74f702d452f3b0ce93dbe80b96c94f97
Alan Bares <alanbares@gmail.com>
2023-10-11 16:55:56 -0700

    Add Breed hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 const ANIMALS = ["bird","cat", "dog", "rabbit", "reptile"];
+const BREEDS = [];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  
   return (
     <div className="search-params">
       <form action="">
@@ -22,11 +25,28 @@ const SearchParams = () => {
           value = {animal}
           onChange={(e) => {
             setAnimal(e.target.value)
+            setBreed("");
           }}
           >
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select 
+          id="breed"
+          disabled={BREEDS.length === 0}
+          value = {breed}
+          onChange={(e) => {
+            setBreed(e.target.value)
+          }}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>

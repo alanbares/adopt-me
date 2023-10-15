535f302f972d695b969383dce24972c73c56b740
Alan Bares <alanbares@gmail.com>
2023-10-15 15:02:37 -0700

    Creat modal

--- src/Details.jsx
+++ src/Details.jsx
@@ -32,10 +32,10 @@ const Details = () => {
   );
 };
 
-function DetailsErrorBoundary() {
+function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
-      <Details />
+      <Details {...props}/>
     </ErrorBoundary>
   )
 }
